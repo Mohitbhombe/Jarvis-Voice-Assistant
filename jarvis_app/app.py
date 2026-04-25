99d4601af6e70ee6e6c583fe1671046fbaaa3918
mohit bhombe <bhombemohit@gmail.com>
2026-04-25 20:45:46 +0530

    Configure for cloud deployment

--- jarvis_app/app.py
+++ jarvis_app/app.py
@@ -217,4 +217,5 @@ if __name__ == "__main__":
     threading.Timer(1.0, lambda: webbrowser.open("http://127.0.0.1:5000")).start()
     
     # Start the Flask app
-    app.run(host="127.0.0.1", port=5000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
